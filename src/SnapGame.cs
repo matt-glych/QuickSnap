7b9fc574942d98fe98d1455bbd1ac34a1189e4fd
Kientrung <kientrung2509@gmail.com>
2020-03-10 16:27:47 +1100

    add player .....

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -4,29 +4,47 @@ using CardGames.GameLogic;
 
 namespace CardGames
 {
-    public class SnapGame
-    {
-        public static void LoadResources()
-        {
-            Bitmap cards;
-            cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
-        }
+	public class SnapGame
+	{
+		public static void LoadResources()
+		{
+			Bitmap cards;
+			cards = SwinGame.LoadBitmapNamed("Cards", "Cards.png");
+			SwinGame.BitmapSetCellDetails(cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+		}
 
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
-		{
+		private static void HandleUserInput(Snap myGame) {
 			//Fetch the next batch of UI interaction
 			SwinGame.ProcessEvents();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
+			if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.FlipNextCard();
+			}
+			if (myGame.IsStarted)
+			{
+				if ( SwinGame.KeyTyped(KeyCode.vk_LSHIFT)&&
+						SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+				{
+					//ToDo: add sound effects
+				}
+				else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
+				{
+					myGame.PlayerHit(0);
+				}
+				else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+				{
+					myGame.PlayerHit(1);
+				}
 			}
 		}
+
+			
+        
 
 		/// <summary>
 		/// Draws the game to the Window.
